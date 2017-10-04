05fe273eca5b950c8c3e88758bf36163f3f975e9
John Cruickshank <johncruickshank87@gmail.com>
2017-10-04 20:29:04 +0100

    add more music to test fully

--- db/console.rb
+++ db/console.rb
@@ -12,9 +12,13 @@ artist1 = Artist.new({
 artist2 = Artist.new({
   'name' => 'The Weeknd'
   })
+artist3 = Artist.new({
+  'name' => 'Jay Z'
+  })
 
 artist1.save()
 artist2.save()
+artist3.save()
 
 album1 = Album.new({
   'title' => 'The Life of Pablo',
@@ -27,9 +31,21 @@ album2 = Album.new({
     'genre' => 'Hip Hop',
     'artist_id' => artist2.id
     })
+album3 = Album.new({
+    'title' => 'The Blueprint',
+    'genre' => 'Hip Hop',
+    'artist_id' => artist3.id
+    })
+album4 = Album.new({
+    'title' => 'Late Registration',
+    'genre' => 'Hip Hop',
+    'artist_id' => artist1.id
+    })
 
 album1.save()
 album2.save()
+album3.save()
+album4.save()
 
 
 binding.pry
